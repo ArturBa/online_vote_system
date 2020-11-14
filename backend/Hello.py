202e011aac21b5cb1a4123f9fd42a5fc4f75c976
Artur Bauer <bauerart1@gmail.com>
2020-11-14 11:35:21 +0100

    Add backend

--- backend/Hello.py
+++ backend/Hello.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/status', methods = ['POST', 'GET'])
+@app.route('/status', methods = ['GET'])
 def index():
     return jsonify(electionsState = 'ACTIVE', startTime = '1970-01-01 00:00:00', endTime = '2020-01-01 00:00:00')
 
--- swagger/server.js
+++ swagger/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-var cors = require('cors');
+const bodyParser = require("body-parser");
+var cors = require("cors");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/status', (req, res) => {
+app.get("/status", (req, res) => {
   res.send({
-    electionsState: 'ACTIVE',
-    startDate: '20-02-2020',
-    endDate: '30-02-2020',
+    electionsState: "ACTIVE",
+    startDate: "20-02-2020",
+    endDate: "30-02-2020",
   });
 });
 
